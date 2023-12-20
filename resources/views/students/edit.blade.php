d5e289097475ea0e6194db1b69a0cd8046d07fe7
Nasrul Hazim Bin Mohamad <nasrulhazim.m@gmail.com>
2023-12-20 11:34:32 +0800

    wip

--- resources/views/students/edit.blade.php
+++ resources/views/students/edit.blade.php
@@ -29,15 +29,27 @@
                     </div>
 
                     <div class="col-span-6 sm:col-span-4 mb-4">
-                        <x-label for="password" value="{{ __('New Password') }}" />
-                        <x-input id="password" type="password" class="mt-1 block w-full" name="password" autocomplete="new-password" />
-                        <x-input-error for="password" class="mt-2" />
+                        <x-label for="ic" value="{{ __('IC') }}" />
+                        <x-input id="ic" name="ic" type="text" class="mt-1 block w-full"
+                            value="{{ $student->ic }}"
+                            required autocomplete="ic" />
+                        <x-input-error for="ic" class="mt-2" />
                     </div>
 
                     <div class="col-span-6 sm:col-span-4 mb-4">
-                        <x-label for="password_confirmation" value="{{ __('Confirm Password') }}" />
-                        <x-input id="password_confirmation" type="password" class="mt-1 block w-full" name="password_confirmation" autocomplete="new-password" />
-                        <x-input-error for="password_confirmation" class="mt-2" />
+                        <x-label for="phone_number" value="{{ __('Phone Number') }}" />
+                        <x-input id="phone_number" name="phone_number" type="text" class="mt-1 block w-full"
+                        value="{{ $student->phone_number }}"
+                            required autocomplete="phone_number" />
+                        <x-input-error for="phone_number" class="mt-2" />
+                    </div>
+
+                    <div class="col-span-6 sm:col-span-4 mb-4">
+                        <x-label for="address" value="{{ __('Address') }}" />
+                        <x-input id="address" name="address" type="text" class="mt-1 block w-full"
+                            value="{{ $student->address }}"
+                            required autocomplete="address" />
+                        <x-input-error for="address" class="mt-2" />
                     </div>
 
                     <x-button>Submit</x-button>
